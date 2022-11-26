53400e67ef6124c5fa4a90dddbfd016666c917f6
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-11-26 15:45:36 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import {Sequelize} from "sequelize";
 
-// const db = new Sequelize('auth_db', 'root', '', {
-//     host: "localhost",
-//     dialect: "mysql"
-// });
-const db = 'mysql://root:CGFwy6eRzM2IMr0yBJTQ@containers-us-west-119.railway.app:5628/railway';
+const db = new Sequelize('auth_db', 'root', '', {
+    host: "localhost",
+    dialect: "mysql"
+});
+
 export default db;
