235dff3c453c86b842c826da4749440e3aedb2d3
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-11-26 23:07:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(session({
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:8080'
 }));
 app.use(express.json());
 app.use(UserRoute);
